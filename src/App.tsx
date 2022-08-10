7ad88343cc55cb9e440b540fa0212cd28b6cc386
nechamish <nechami77@gmail.com>
2022-08-10 16:08:10 +0300

    show details systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 
 import * as React from 'react'
-import './App.css';
 import List from './components/user';
 import Home from './components/home-system';
 
-import { Route, Router, Routes } from 'react-router-dom';
 
 
 function App() {
     return (
     <div className="app">
       
-          <List/>
+          <Home/>
     </div>
       
     );
--- src/components/home-system.tsx
+++ src/components/home-system.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import {useEffect, useState} from "react"
-
+import { useEffect, useState } from "react"
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import axios from "axios";
 
-interface System{
+interface System {
     _id: string;
     uid: string;
     topic: string;
@@ -12,41 +17,63 @@ interface System{
     managerUid: string;
     description: string;
     communicationDetails: object;
-    _v:number
+    _v: number
 }
-const Home=()=>{
+const Home = () => {
 
     const [systems, setSystems] = useState<System[]>([]);
-    
-    useEffect(()=>{
-        getAllSystem();
-     } ,[]);
- 
-     const getAllSystem= async () =>{
-         const data = await axios.get<System[]>('http://localhost:3333/system');
-         console.log(data);
-         setSystems(data.data);
-     }
-     
 
-    const renderHome=(): JSX.Element[]=>{
-        return systems.map(system=>{
-            return(
-                <div>
-                    <th>id: {system._id}</th>  
-                    <tr>firstName: {system.managerUid}</tr>
-                    <tr>lastName: {system.objectName}</tr>
-                    <tr>email: {system.topic}</tr>
-                    <br/>
-                </div>
-            )
-        })
+    useEffect(() => {
+        getAllSystem();
+    }, []);
+
+    const getAllSystem = async () => {
+        const data = await axios.get<System[]>('http://localhost:3333/system');
+        console.log(data);
+        setSystems(data.data);
     }
-   return(
-    <ul>
-        {renderHome()}
-    </ul>
-   )
+
+
+    const renderHome = (): JSX.Element[] => {
+        return systems.map(system => {
+            return (
+                <div>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="../images/צילום מסך 2022-08-10 144018.png"
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                <div>{system.topic}</div>
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                <div>
+                                    <th>id: {system._id}</th>
+                                    <tr>managerUid: {system.managerUid}</tr>
+                                    <tr>objectName: {system.objectName}</tr>
+                                    <tr>description:{system.description}</tr>
+                                    <br />
+                                </div>
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Show user</Button>
+
+                        </CardActions>
+                    </Card>
+                </div>
+            );
+        });
+    }
+
+    return (
+        <ul>
+            {renderHome()}
+        </ul>
+    )
 }
 
 export default Home;
